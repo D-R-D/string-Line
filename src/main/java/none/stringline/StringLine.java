3b9da296237149b5b4f29def96cb9f29c8eb71de
DDD <77850088+D-R-D@users.noreply.github.com>
2022-02-10 00:36:09 +0900

    ・mcp1.18.1のapiを使うように更新 ・タイポ修正

--- src/main/java/none/stringline/StringLine.java
+++ src/main/java/none/stringline/StringLine.java
@@ -21,7 +21,6 @@ public final class StringLine extends JavaPlugin implements Listener
     //タイマーの生死確認用の真偽値
     boolean Boolean = false , savlean = false , firlean = false;
     String name;
-    //true = living , false = dead
 
     //
     /*プラグイン起動時に呼び出されるイベント*/
@@ -44,25 +43,19 @@ public final class StringLine extends JavaPlugin implements Listener
         }
         catch(FileNotFoundException e){ System.out.println(e.getMessage()); }
         catch (IOException e) { e.printStackTrace(); }
-        eventsender(name+":started");
-        sinssender(names);
+        try {
+            eventsender(name+":started");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            sinssender(names);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
-        //senderでbotに通知、getLoggerでログを打つ
-
-        /*
-        Thread r_thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                receiver();
-            }
-        });
-        r_thread.start();
-         */
-
-        //sender("status:true");
-        //sender("[plugin]:サーバーが開きました。");
-        //sender("[plugin]:プレイヤーのログインを待っています。ログインがない場合、10分後にサーバーをシャットダウンします。");
+        //getLoggerでログを打つ
         getLogger().info("タイマーを開始します。");
 
         //自動シャットダウン用のタイマーを起動、生死確認用の真偽値を真にしておく
@@ -81,11 +74,16 @@ public final class StringLine extends JavaPlugin implements Listener
     public void onDisable()
     {
         String named = "container:dead:"+name;
-        eventsender(name+":stopped");
-        sinssender(named);
-        //sender("status:false");
-        //sender("[plugin]:サーバーが終了しました。");
-        //sender("[plugin]:サーバーの再起動には、コマンド/startを使用してください。");
+        try {
+            eventsender(name+":stopped");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            sinssender(named);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     //
     /*プラグイン終了時に呼び出されるイベント*/
@@ -96,22 +94,29 @@ public final class StringLine extends JavaPlugin implements Listener
     //
     @EventHandler
     public void onLogoutEvent(PlayerQuitEvent event) {
-        //botへ通知(邪魔だったらここを消す)
-        //sender("[plugin]:プレイヤーがログアウトしました。");
-
         //オンラインのプレイヤーが0になったらセーブ かつ timerが死んでるときに新しくtimerを起動する。
         //生死判定用真偽値の更新を忘れずに
         Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
             public void run() {
                 if (Bukkit.getOnlinePlayers().isEmpty()) {
                     getLogger().info("ワールドのセーブを開始します。");
-                    sinssender("alert container_name " + name + " : プレイヤー数が0になりました。ワールドのセーブを開始します。");
-                    Save();
+                    try {
+                        sinssender("alert container_name " + name + " : プレイヤー数が0になりました。ワールドのセーブを開始します。");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        Save();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                     if (!Boolean) {
                         Boolean = true;
-                        //sender("[plugin]:オンラインプレイヤーが0です。シャットダウンを開始します。");
-                        //sender("[plugin]:サーバーのシャットダウンを開始しています。10分後にサーバーをシャットダウンします。");
-                        sinssender("alert container_name " + name + " : 新しくログインがない場合10分後にシャットダウンされます。");
+                        try {
+                            sinssender("alert container_name " + name + " : 新しくログインがない場合10分後にシャットダウンされます。");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                         getLogger().info("タイマーを開始します。");
                         timers();
                     }
@@ -130,7 +135,7 @@ public final class StringLine extends JavaPlugin implements Listener
     public void onLoginEvent(PlayerJoinEvent event)
     {
         //既に1人以上ログインしているため生死確認用真偽値の確認だけでok
-        //実際にtimerを停止する部分はtimerに組み込み済みだから気にしないで
+        //実際にtimerを停止する部分はtimerに組み込み済み
 
         firlean = true;
         savlean = true;
@@ -147,8 +152,7 @@ public final class StringLine extends JavaPlugin implements Listener
     //
     /*セーブ*/
     //
-    private void Save()
-    {
+    private void Save() throws IOException {
         getLogger().info("サーバーをセーブ中です…");
         savePlayers();
         saveWorld();
@@ -164,7 +168,8 @@ public final class StringLine extends JavaPlugin implements Listener
     private void saveWorld()
     {
         List<World> worlds = this.getServer().getWorlds();
-        for (World world : worlds) {
+        for (World world : worlds)
+        {
             world.save();
         }
     }
@@ -184,14 +189,12 @@ public final class StringLine extends JavaPlugin implements Listener
             //初回だけ実行される部分
             int i = 0;
 
-            //
             /*タイマーで毎回回される部分*/
-            //
             @Override
             public void run() {
                 i++;
                 //オンラインのプレイヤー数が0以上の時タイマーを終了させる
-                //生死確認用真偽値の更新を忘れずに
+                //生死確認用真偽値の更新
                 if (!Bukkit.getOnlinePlayers().isEmpty()) {
                     getLogger().info("[plugin.timers]:サーバーのシャットダウンを停止しました。");
                     Boolean = false;
@@ -201,7 +204,7 @@ public final class StringLine extends JavaPlugin implements Listener
                 }
 
                 //10回目のループ(10分後)にサーバーをシャットダウンする
-                //生死確認用真偽値の更新を忘れずに
+                //生死確認用真偽値の更新
                 if (i == 10) {
                     getLogger().info("[plugin.timers]:10分経過しました。サーバーをシャットダウンします。");
                     getServer().shutdown();
@@ -212,110 +215,49 @@ public final class StringLine extends JavaPlugin implements Listener
                 }
                 getLogger().info("[plugin.timers]:" + i + "分経過しました。");
             }
-            //
-            /*タイマーで毎回回される部分*/
-            //
+            /**/
         };
-
         //タイマーの設定
         timer.scheduleAtFixedRate(task, 60000, 60000);
-        //タイマーの設定
     }
-
-    /*
-    public void sub_timers() {
-        //バグ回避のためタイマーは毎回新しく作る
-        Timer timer = new Timer();
-        //タイマーで回すタスク
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                if (Bukkit.getOnlinePlayers().isEmpty() && firlean)
-                {
-                    if(savlean) {
-                        savlean = false;
-                        getLogger().info("[plugin]:ワールドのセーブを開始します。");
-                        sinssender("alert container_name " + name + " : プレイヤー数が0になりました。ワールドのセーブを開始します。");
-                        Save();
-                    }
-                    if(!Boolean) {
-                        Boolean = true;
-                        sinssender("alert container_name " + name + " : 新しくログインがない場合10分後にシャットダウンされます。");
-                        getLogger().info("[plugin]:タイマーを開始します。");
-                        timers();
-                    }
-                }
-            }
-        };
-        timer.scheduleAtFixedRate(task, 1000, 1000);
-    }
-    */
-
     //
-    /*botにudp通信でデータを渡す*/
+    /**/
     //
-    /*
-    public void sender(String str)
-    {
-        //strをbyte配列に変換
-        byte[] data;
-        data = str.getBytes(StandardCharsets.UTF_8);
-        //接続用のソケットを作成
-        DatagramSocket sock = null;
-        try { sock = new DatagramSocket(); }
-        catch (SocketException e) { e.printStackTrace();}
-        //パケットを作成し、udpで送信する。
-        DatagramPacket packet = new DatagramPacket(data,data.length,new InetSocketAddress("127.0.0.1",6001));
-        try { sock.send(packet); }
-        catch (IOException e) { e.printStackTrace(); }
-        //最後にお片付けして終了
-        sock.close();
-    }
-    */
-    //
-    /*botにudp通信でデータを渡す*/
-    //
-
 
     //
     /*sinsにudp送信*/
     //
-    public void sinssender(String str)
-    {
+
+    //sins,bungeecordコマンド用の接続
+    public void sinssender(String str) throws IOException {
         //strをbyte配列に変換
         byte[] data;
         data = str.getBytes(StandardCharsets.UTF_8);
         //接続用のソケットを作成
-        DatagramSocket sock = null;
-        try { sock = new DatagramSocket(); }
-        catch (SocketException e) { e.printStackTrace();}
+        DatagramSocket sock = new DatagramSocket();
         //パケットを作成し、udpで送信する。
         DatagramPacket packet = new DatagramPacket(data,data.length,new InetSocketAddress("127.0.0.1",6011));
-        try { sock.send(packet); }
-        catch (IOException e) { e.printStackTrace(); }
+        sock.send(packet);
         //最後にお片付けして終了
         sock.close();
 
-        getLogger().info("udp sended");
+        getLogger().info("udp sent");
     }
 
-    public void eventsender(String str)
-    {
+    //ステータス管理用の接続
+    public void eventsender(String str) throws IOException {
         //strをbyte配列に変換
         byte[] data;
         data = str.getBytes(StandardCharsets.UTF_8);
         //接続用のソケットを作成
-        DatagramSocket sock = null;
-        try { sock = new DatagramSocket(); }
-        catch (SocketException e) { e.printStackTrace();}
+        DatagramSocket sock = new DatagramSocket();
         //パケットを作成し、udpで送信する。
         DatagramPacket packet = new DatagramPacket(data,data.length,new InetSocketAddress("127.0.0.1",7011));
-        try { sock.send(packet); }
-        catch (IOException e) { e.printStackTrace(); }
+        sock.send(packet);
         //最後にお片付けして終了
         sock.close();
 
-        getLogger().info("event sended");
+        getLogger().info("event sent");
     }
     //
     /*sinsにudp送信*/
@@ -325,30 +267,18 @@ public final class StringLine extends JavaPlugin implements Listener
     //
     /*sinsからデータを受け取る*/
     //
-    public void receiver() {
+
+    //sinsからのサーバー操作用の接続
+    public void receiver() throws IOException {
         //データ受信
         byte[] data = new byte[1024];
-        DatagramSocket sock = null;
-        try {
-            sock = new DatagramSocket(6011);
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
+        DatagramSocket sock = new DatagramSocket(7001);
         DatagramPacket packet = new DatagramPacket(data, data.length);
 
-        while (true) {
-            try {
-                sock.receive(packet);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            String str = null;
-            try {
-                str = new String(Arrays.copyOf(packet.getData(), packet.getLength()), "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-
+        while (true)
+        {
+            sock.receive(packet);
+            String str = new String(Arrays.copyOf(packet.getData(), packet.getLength()), "UTF-8");
             getLogger().info(str);
 
             //データごとに処理
